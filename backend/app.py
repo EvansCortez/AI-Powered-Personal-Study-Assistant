d3268f61f275fbac65df3223386f28663bc8f9f7
EvansCortez <157630075+EvansCortez@users.noreply.github.com>
2025-05-15 16:36:38 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,8 @@
+from flask import Flask, request, jsonify
+from routes.upload import upload_bp
 
+app = Flask(__name__)
+app.register_blueprint(upload_bp, url_prefix="/upload")
+
+if __name__ == "__main__":
+    app.run(debug=True)
